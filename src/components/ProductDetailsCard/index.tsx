485aee91cc53579aa4c80e286facbc3ae21fe764
kelvin-feltrin <kelvin-798@hotmail.com>
2025-09-16 15:44:42 -0300

    Passando um objeto produto como Prop

--- src/components/ProductDetailsCard/index.tsx
+++ src/components/ProductDetailsCard/index.tsx
@@ -1,20 +1,23 @@
 import './styles.css';
-import computerImg from '../../assets/computer.png';
-import ProductCategory from '../ProductCategory';
 
-export default function ProductDetailsCard() {
+import ProductCategory from '../ProductCategory';
+import type { ProductDTO } from '../../models/product';
+
+type Props = {
+    product: ProductDTO;
+}
+
+export default function ProductDetailsCard({ product } : Props) {
     return (
         <div className="dsc-card dsc-mb20">
             <div className="dsc-product-details-top dsc-line-bottom">
-                <img src={computerImg} alt="Computador" />
+                <img src={product.imgUrl} alt={product.name} />
             </div>
 
             <div className="dsc-product-details-bottom">
-                <h3>R$ 5000,00</h3>
-                <h4>Computador Gamer XT</h4>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem alias commodi iste minus nobis sed nisi, consectetur earum perferendis quod praesentium culpa minima doloremque quaerat, facilis voluptate officia a et.
-                </p>
+                <h3>R$ {product.price.toFixed(2)}</h3>
+                <h4>{product.name}</h4>
+                <p>{product.description}</p>
                 <div className="dsc-category-container">
                     <ProductCategory name="Eletrônicos" />
                     <ProductCategory name="Computadores" />
