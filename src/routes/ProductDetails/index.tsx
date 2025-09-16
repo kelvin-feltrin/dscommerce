b526ec94c5578c0cb3fd6548fab9509ea0bb82a2
kelvin-feltrin <kelvin-798@hotmail.com>
2025-09-16 15:51:47 -0300

    Renderizando colecoes dinamicamente no React

--- src/routes/ProductDetails/index.tsx
+++ src/routes/ProductDetails/index.tsx
@@ -19,6 +19,10 @@ const product: ProductDTO = {
         {
             id: 3,
             name: "Computadores"
+        },
+        {
+            id: 4,
+            name: "Importados"
         }
     ]
 }
