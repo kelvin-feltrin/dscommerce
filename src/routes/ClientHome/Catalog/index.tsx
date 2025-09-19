2002439cf8a033bdfd8c3da2be3d8142c2f4820b
kelvin-feltrin <kelvin-798@hotmail.com>
2025-09-19 00:15:50 -0300

    Renderizando catalogo de produtos dinamicamente

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -3,29 +3,9 @@ import './styles.css';
 import SearchBar from '../../../components/SearchBar';
 import CatalogCard from '../../../components/CatalogCard';
 import ButtonNextPage from '../../../components/ButtonNextPage';
-import type { ProductDTO } from '../../../models/product';
+import * as productService from '../../../services/product-service';
 
-const product: ProductDTO = {
-    id: 2,
-    name: "Smart TV",
-    description: "Essa TV é muito bonita",
-    imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/refs/heads/master/backend/img/2-big.jpg",
-    price: 2500.99,
-    categories: [
-        {
-            id: 2,
-            name: "Eletrônicos"
-        },
-        {
-            id: 3,
-            name: "Computadores"
-        },
-        {
-            id: 4,
-            name: "Importados"
-        }
-    ]
-}
+
 
 export default function Catalog() {
     return (
@@ -34,17 +14,11 @@ export default function Catalog() {
                 <SearchBar />
 
                 <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
+                    {
+                        productService.findAll().map(
+                            product => <CatalogCard key={product.id} product={product} />
+                        )
+                    }
                 </div>
 
                 <ButtonNextPage />
