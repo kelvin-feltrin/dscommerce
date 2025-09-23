f0a8d8b3b15b4894e21a51e3258bf539876b8a82
kelvin-feltrin <kelvin-798@hotmail.com>
2025-09-22 22:48:50 -0300

    Obtendo o carrinho do localStorage

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -1,6 +1,6 @@
 import './styles.css';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import * as cartService from '../../../services/cart-service';
 import { OrderDTO, OrderItemDTO } from '../../../models/order';
 
@@ -13,14 +13,8 @@ const item2: OrderItemDTO = new OrderItemDTO(
 );
 
 export default function Cart() {
-    const cart : OrderDTO = new OrderDTO();
 
-    useEffect(() => {
-        cart.items.push(item1);
-        cart.items.push(item2);
-
-        cartService.saveCart(cart);
-    }, []);
+    const [cart, setCart] = useState<OrderDTO>(cartService.getCart());
 
     return (
         <main>
--- src/services/cart-service.ts
+++ src/services/cart-service.ts
@@ -4,3 +4,6 @@ import * as cartRepository from '../localstorage/cart-repository';
 export function saveCart(cart: OrderDTO) {
     cartRepository.save(cart);
 }
+export function getCart() : OrderDTO {
+    return cartRepository.get();
+}
