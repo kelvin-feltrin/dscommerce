c0250c55d587e77f9733b964068f390f0a11904c
kelvin-feltrin <kelvin-798@hotmail.com>
2025-09-24 00:03:55 -0300

    Decrementar a quantidade de um item do carrinho

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -19,45 +19,49 @@ export default function Cart() {
         setCart(cartService.getCart());
     }
 
+    function handleDecreaseItem(productId: number) {
+        cartService.decreaseItem(productId);
+        setCart(cartService.getCart());
+    }
+
     return (
         <main>
             <section id="cart-container-section" className="dsc-container">
-                {
-                    cart.items.length === 0
-                    ? (
-                        <div>
-                            <h2 className='dsc-section-title dsc-mb20'>Seu carrinho está vazio</h2>
-                        </div>
-                    )
-                    : (
-                        <div className="dsc-card dsc-mb20">
-                            {
-                                cart.items.map(item => (
-                                    <div key={item.productId} className="dsc-cart-item-container dsc-line-bottom">
-                                        <div className="dsc-cart-item-left">
-                                            <img src={item.imgUrl} alt={item.name} />
-                                            <div className="dsc-cart-item-description">
-                                                <h3>{item.name}</h3>
-                                                <div className="dsc-cart-item-quantity-container">
-                                                    <div className="dsc-cart-item-quantity-btn">-</div>
-                                                    <p>{item.quantity}</p>
-                                                    <div onClick={() => handleIncreaseItem(item.productId)} className="dsc-cart-item-quantity-btn">+</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="dsc-cart-item-right">
-                                            R$ {item.subTotal.toFixed(2)}
+            {
+            cart.items.length === 0
+            ? (
+                <div>
+                    <h2 className='dsc-section-title dsc-mb20'>Seu carrinho está vazio</h2>
+                </div>
+            )
+            : (
+                <div className="dsc-card dsc-mb20">
+                    {
+                        cart.items.map(item => (
+                            <div key={item.productId} className="dsc-cart-item-container dsc-line-bottom">
+                                <div className="dsc-cart-item-left">
+                                    <img src={item.imgUrl} alt={item.name} />
+                                    <div className="dsc-cart-item-description">
+                                        <h3>{item.name}</h3>
+                                        <div className="dsc-cart-item-quantity-container">
+                                            <div onClick={() => handleDecreaseItem(item.productId)} className="dsc-cart-item-quantity-btn">-</div>
+                                            <p>{item.quantity}</p>
+                                            <div onClick={() => handleIncreaseItem(item.productId)} className="dsc-cart-item-quantity-btn">+</div>
                                         </div>
                                     </div>
-                                ))
-                            }
-
-                            <div className="dsc-cart-total-container">
-                                <h3>R$ {cart.total.toFixed(2)}</h3>
+                                </div>
+                                <div className="dsc-cart-item-right">
+                                    R$ {item.subTotal.toFixed(2)}
+                                </div>
                             </div>
-                        </div>
-                    )
-                }
+                        ))
+                    }
+
+                    <div className="dsc-cart-total-container">
+                        <h3>R$ {cart.total.toFixed(2)}</h3>
+                    </div>
+                </div>
+            )}
                 <div className="dsc-btn-page-container">
                     <div className="dsc-btn dsc-btn-blue">
                         Finalizar pedido
