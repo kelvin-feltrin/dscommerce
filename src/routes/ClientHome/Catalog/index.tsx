604ecefdd3dc3ec5d15ed571f1403e4103f851a5
kelvin-feltrin <kelvin-798@hotmail.com>
2025-09-25 23:45:08 -0300

    Passando parametros para a requisicao de produtos

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -15,7 +15,7 @@ export default function Catalog() {
     const [products, setProducts] = useState<ProductDTO[]>([]);
 
     useEffect(() => {
-        productService.findAll()
+        productService.findPageRequest(0, "")
             .then(response => {
                 setProducts(response.data.content);
             });
