81d362d8c9cb245e38e652b84335a918bef27fa0
kelvin-feltrin <kelvin-798@hotmail.com>
2025-10-14 23:37:12 -0300

    Estado global do payload do token com Context API

--- src/components/HeaderClient/index.tsx
+++ src/components/HeaderClient/index.tsx
@@ -4,8 +4,13 @@ import { Link } from 'react-router-dom';
 import CartIcon from '../CartIcon';
 import iconAdmin from '../../assets/admin.svg';
 import * as authService from '../../services/auth-service';
+import { useContext } from 'react';
+import { ContextToken } from '../../utils/context-token';
 
 export default function HeaderClient() {
+
+    const { contextTokenPayload } = useContext(ContextToken);
+
     return (
         <header className="dsc-header-client">
             <nav className="dsc-container">
@@ -15,6 +20,7 @@ export default function HeaderClient() {
                 <div className="navbar-right">
                     <div className="dsc-menu-items-container">
                         {
+                            contextTokenPayload &&
                             authService.hasAnyRoles(['ROLE_ADMIN']) &&
                             <Link to="/admin">
                                 <div className="dsc-menu-item">
