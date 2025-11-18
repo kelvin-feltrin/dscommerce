08b2384f6ddea0f4998993b73650fdfab196537f
Kelvin <kelvin-798@hotmail.com>
2025-11-18 13:46:20 -0300

    Preparando o formData do formulario de produtos

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -1,33 +1,77 @@
-import { Link } from 'react-router-dom';
 import './styles.css';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import FormInput from '../../../components/FormInput';
+import * as forms from '../../../utils/forms';
 
 export default function ProductForm() {
+
+    const [formData, setFormData] = useState<any>({
+        name: {
+            value: "",
+            id: "name",
+            name: "name",
+            type: "text",
+            placeholder: "Nome",
+        },
+        price: {
+            value: "",
+            id: "price",
+            name: "price",
+            type: "number",
+            placeholder: "Preço",
+        },
+        imgUrl: {
+            value: "",
+            id: "imgUrl",
+            name: "imgUrl",
+            type: "text",
+            placeholder: "Imagem",
+        }
+    })
+
+    function handleInputChange(event: any) {
+        setFormData(forms.update(formData, event.target.name, event.target.value));
+    }
+
     return (
         <main>
-        <section id="product-form-section" className="dsc-container">
-            <div className="dsc-product-form-container">
-                <form className="dsc-card dsc-form">
-                    <h2>Dados do produto</h2>
-                    <div className="dsc-form-controls-container">
-                        <div>
-                            <input className="dsc-form-control" type="text" placeholder="Nome" />
+            <section id="product-form-section" className="dsc-container">
+                <div className="dsc-product-form-container">
+                    <form className="dsc-card dsc-form">
+                        <h2>Dados do produto</h2>
+                        <div className="dsc-form-controls-container">
+                            <div>
+                                <FormInput
+                                    {...formData.name}
+                                    className="dsc-form-control"
+                                    onChange={handleInputChange}
+                                />
+                            </div>
+                            <div>
+                                <FormInput
+                                    {...formData.price}
+                                    className="dsc-form-control"
+                                    onChange={handleInputChange}
+                                />
+                            </div>
+                            <div>
+                                <FormInput
+                                    {...formData.imgUrl}
+                                    className="dsc-form-control"
+                                    onChange={handleInputChange}
+                                />
+                            </div>
                         </div>
-                        <div>
-                            <input className="dsc-form-control" type="text" placeholder="Preço" />
+                        <div className="dsc-product-form-buttons">
+                            <Link to="/admin/products">
+                                <button type="submit" className="dsc-btn dsc-btn-white">Cancelar</button>
+                            </Link>
+                            <button type="submit" className="dsc-btn dsc-btn-blue">Salvar</button>
                         </div>
-                        <div>
-                            <input className="dsc-form-control" type="text" placeholder="Imagem" />
-                        </div>
-                    </div>
-                    <div className="dsc-product-form-buttons">
-                        <Link to="/admin/products">
-                            <button type="submit" className="dsc-btn dsc-btn-white">Cancelar</button>
-                        </Link>
-                        <button type="submit" className="dsc-btn dsc-btn-blue">Salvar</button>
-                    </div>
-                </form>
-            </div>
-        </section>
-    </main>
+                    </form>
+                </div>
+            </section>
+        </main>
     );
 }
