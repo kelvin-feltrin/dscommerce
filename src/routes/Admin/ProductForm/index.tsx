0d31a9963c2856577cd4d6e31a38654ffa8d645a
Kelvin <kelvin-798@hotmail.com>
2025-12-02 21:20:21 -0300

    Diferenciando a criacao e edicao no formulario

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -1,10 +1,15 @@
 import './styles.css';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import FormInput from '../../../components/FormInput';
 import * as forms from '../../../utils/forms';
+import * as productService from '../../../services/product-service';
 
 export default function ProductForm() {
+
+    const params = useParams();
+
+    const isEditing = params.productId !== 'create';
 
     const [formData, setFormData] = useState<any>({
         name: {
@@ -33,6 +38,15 @@ export default function ProductForm() {
     function handleInputChange(event: any) {
         setFormData(forms.update(formData, event.target.name, event.target.value));
     }
+
+    useEffect(() => {
+        if (isEditing) {
+            productService.findById(Number(params.productId))
+                .then(response => {
+                    console.log(response.data);
+                })
+        }
+    }, []);
 
     return (
         <main>
