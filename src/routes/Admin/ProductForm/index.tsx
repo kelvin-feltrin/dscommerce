62cff9cb2215374bd0079d346d25d0929d00d479
Kelvin <kelvin-798@hotmail.com>
2025-12-02 22:02:05 -0300

    CSS para mostrar os erros

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -20,7 +20,7 @@ export default function ProductForm() {
             placeholder: "Nome",
         },
         price: {
-            value: 200,
+            value: "",
             id: "price",
             name: "price",
             type: "number",
@@ -40,14 +40,12 @@ export default function ProductForm() {
     })
 
     function handleInputChange(event: any) {
-        setFormData(forms.update(formData, event.target.name, event.target.value));
+        const dataUpdated = forms.update(formData, event.target.name, event.target.value);
+        const dataValidated = forms.validate(dataUpdated, event.target.name);
+        setFormData(dataValidated);
     }
 
     useEffect(() => {
-    
-        const obj = forms.validate(formData, "price");
-        console.log(obj);
-
         if (isEditing) {
             productService.findById(Number(params.productId))
                 .then(response => {
@@ -70,6 +68,9 @@ export default function ProductForm() {
                                     className="dsc-form-control"
                                     onChange={handleInputChange}
                                 />
+                                <div className='dsc-form-error'>
+                                    {formData.name.message}
+                                </div>
                             </div>
                             <div>
                                 <FormInput
@@ -77,6 +78,9 @@ export default function ProductForm() {
                                     className="dsc-form-control"
                                     onChange={handleInputChange}
                                 />
+                                <div className='dsc-form-error'>
+                                    {formData.price.message}
+                                </div>
                             </div>
                             <div>
                                 <FormInput
