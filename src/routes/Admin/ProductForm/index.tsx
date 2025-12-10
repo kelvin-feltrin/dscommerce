30579b2429d7efc1a98a8dfef88a4a54327808d5
Kelvin <kelvin-798@hotmail.com>
2025-12-09 22:32:15 -0300

    Integrando Select com formulario

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -6,8 +6,8 @@ import * as forms from '../../../utils/forms';
 import * as productService from '../../../services/product-service';
 import * as categoryService from '../../../services/category-service';
 import FormTextArea from '../../../components/FormTextArea';
-import Select from 'react-select';
 import type { CategoryDTO } from '../../../models/category';
+import FormSelect from '../../../components/FormSelect';
 
 export default function ProductForm() {
 
@@ -57,6 +57,16 @@ export default function ProductForm() {
                 return /^.{10,}$/.test(value);
             },
             message: "A descrição deve ter pelo menos 10 caracteres"
+        },
+        categories: {
+            value: [],
+            id: "categories",
+            name: "categories",
+            placeholder: "Categorias",
+            validation: function (value: CategoryDTO[]) {
+                return value.length > 0;
+            },
+            message: "Escolha ao menos uma categoria"
         }
     })
 
@@ -134,12 +144,22 @@ export default function ProductForm() {
                                 </div>
                             </div>
                             <div>
-                                <Select 
-                                    options={categories} 
+                                <FormSelect 
+                                    {...formData.categories}
+                                    className="dsc-form-control"
+                                    options={categories}
+                                    onChange={(obj: any) => {
+                                        const newFormData = forms.updateAndValidate(formData, "categories", obj);
+                                        setFormData(newFormData);
+                                    }} 
+                                    onTurnDirty={handleTurnDirty}
                                     isMulti
                                     getOptionLabel={(obj: any) => obj.name}
                                     getOptionValue={(obj: any) => String(obj.id)}
                                 />
+                                <div className='dsc-form-error'>
+                                    {formData.categories.message}
+                                </div>
                             </div>
                         </div>
                         <div className="dsc-product-form-buttons">
