d092d191e7afef68f51fe7745c91d9d2641b50b5
Kelvin <kelvin-798@hotmail.com>
2025-12-09 22:13:56 -0300

    React Select buscando categories

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -4,13 +4,18 @@ import { useEffect, useState } from 'react';
 import FormInput from '../../../components/FormInput';
 import * as forms from '../../../utils/forms';
 import * as productService from '../../../services/product-service';
+import * as categoryService from '../../../services/category-service';
 import FormTextArea from '../../../components/FormTextArea';
+import Select from 'react-select';
+import type { CategoryDTO } from '../../../models/category';
 
 export default function ProductForm() {
 
     const params = useParams();
 
     const isEditing = params.productId !== 'create';
+
+    const [categories, setCategories] = useState<CategoryDTO[]>([]);
 
     const [formData, setFormData] = useState<any>({
         name: {
@@ -62,6 +67,13 @@ export default function ProductForm() {
     function handleTurnDirty(name: string) {
         setFormData(forms.dirtyAndValidate(formData, name));
     }
+
+    useEffect(() => {
+        categoryService.findAllRequest()
+            .then(response => {
+                setCategories(response.data);
+            })
+    }, []);
 
     useEffect(() => {
         if (isEditing) {
@@ -120,6 +132,14 @@ export default function ProductForm() {
                                 <div className='dsc-form-error'>
                                     {formData.description.message}
                                 </div>
+                            </div>
+                            <div>
+                                <Select 
+                                    options={categories} 
+                                    isMulti
+                                    getOptionLabel={(obj: any) => obj.name}
+                                    getOptionValue={(obj: any) => String(obj.id)}
+                                />
                             </div>
                         </div>
                         <div className="dsc-product-form-buttons">
