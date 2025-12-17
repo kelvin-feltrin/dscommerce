b23663be8150cacd3c1a6d5ea3a6db7415d24b92
Kelvin <kelvin-798@hotmail.com>
2025-12-16 21:20:41 -0300

    Mostrando erro global no formulário de login

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -13,6 +13,8 @@ export default function Login() {
 
     const navigate = useNavigate();
 
+    const [submitReponseFail, setSubmitReponseFail] = useState(false);
+
     const [formData, setFormData] = useState<any>({
         username: {
             value: "",
@@ -36,14 +38,23 @@ export default function Login() {
 
     function handleSubmit(event: any) {
         event.preventDefault();
+
+        setSubmitReponseFail(false);
+
+        const formDataValidated = forms.dirtyAndValidateAll(formData);
+        if (forms.hasAnyInvalid(formDataValidated)) {
+            setFormData(formDataValidated);
+            return;
+        }
+
         authService.loginRequest(forms.toValues(formData))
             .then(response => {
                 authService.saveAccessToken(response.data.access_token);
                 setContextTokenPayload(authService.getAccessTokenPayload());
                 navigate("/cart");
             })
-            .catch(error => {
-                console.log("Erro no login", error)
+            .catch(() => {
+                setSubmitReponseFail(true);
             })
     }
 
@@ -82,6 +93,14 @@ export default function Login() {
                                 />
                             </div>
                         </div>
+
+                        {
+                            submitReponseFail &&
+                            <div className='dsc-form-global-error'>
+                                Usuário ou senha inválidos
+                            </div>
+                        }
+
                         <div className="dsc-login-form-buttons dsc-mt20">
                             <button type="submit" className="dsc-btn dsc-btn-blue">Entrar</button>
                         </div>
