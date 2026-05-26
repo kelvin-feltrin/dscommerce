c2bfd7646fe6cbc08f3e3b7e1ac94ed9d9f999d5
Kelvin <kelvin-798@hotmail.com>
2026-05-25 22:09:47 -0300

    Ajustes adicionais para o Netlify

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
-import { createRoot } from 'react-dom/client'
 import './index.css'
+
+import React from 'react';
 import App from './App.tsx'
+import { createRoot } from 'react-dom/client'
+
+window.React = React;
 
 createRoot(document.getElementById('root')!).render(
   <App />
